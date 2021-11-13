9677dc6e5534c6266b2e8c4e1e091bb78e881b4a
alexayca <90019860+alexayca@users.noreply.github.com>
2021-11-12 21:34:22 -0600

    06 - Metodo constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -5,8 +5,23 @@ public class Doctor
     int id;
     String name;
     String speciality;
-
-    //Comportamientos
+    
+    // Metodo constructor
+    Doctor(/*parametros*/)
+    {
+    	// Instrucciones que se ejecutan al instanciar 
+    	// un nuevo objeto con la clase Doctor
+    	System.out.println("Construyendo el objeto Doctor sin parametros");
+    }
+    
+    // Metodo constructor sobrecargado
+    Doctor(String name)
+    {
+    	System.out.println("El nombre del Doctor asignado es: " + name + " (con parametros) ");
+    	// Para modificar la variable 'name' deberia asignarse con this. Pass by value
+    }
+    
+    // Metodos
     public void showName()
     {
         System.out.println(name);
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// declaranto el objeto <- Instanciado el objeto
+		// declarando el objeto <- Instanciado el objeto con el MetodoConstructor();
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro  Vaccuno";
+        
+        Doctor myMedical = new Doctor("Susana");
+        
         myDoctor.showName();
+        
+        // El alcance de la variable esta limitado por lo que retorna NULL.
+        myMedical.showName();
 
 	}
 
