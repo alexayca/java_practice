54aed852f6bed236815bc2bb30f8db4e3a114fd3
alexayca <90019860+alexayca@users.noreply.github.com>
2021-11-13 01:06:37 -0600

    13 - Getters y Setters

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,11 @@ public class Main
 		System.out.println(Doctor.id +" . " +miGinecolaga.name +" - " +miGinecolaga.speciality);
 		
 		Patient paciente = new Patient("Mila", "mila@mail.com");
-		System.out.println(paciente.name +" - " +paciente.email);
+		System.out.println(paciente.getName() +" - " +paciente.getEmail());
+		paciente.setWeight(54.6);
+		System.out.println(paciente.getWeight());
+		paciente.setPhoneNumber("12345678");
+		System.out.println(paciente.getPhoneNumber());
 
 	}
 
--- src/Patient.java
+++ src/Patient.java
@@ -1,7 +1,8 @@
 /* * * * * * * 
- * Los Modificadores de Acceso nos ayudan a limitar desde dónde podemos leer o modificar atributos especiales 
- * de nuestras clases. Podemos definir qué variables se pueden leer/editar por fuera de las clases donde fueron creadas. 
- * Esto lo conocemos como Encapsulamiento.
+ * Los Getters y Setters nos permiten leer y escribir (respectivamente) los valores de nuestras variables privadas desde fuera de la clase 
+ * donde fueron creadas. 
+ * Con los Getters obtenemos los datos de las variables.
+ * Los Setters asignamos o cambiamos su valor.
  * * * * * * */
 
 public class Patient {
@@ -21,5 +22,82 @@ public class Patient {
 		this.name = name;
 		this.email = email;
 	}
+
+	public String getWeight() {
+		return weight + " Kg.";
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getHeight() {
+		return height +" m";
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		if (phoneNumber.length() != 8) {
+			System.out.println("El telefono debe tener 8 digitos");
+		}else if(phoneNumber.length() == 8)
+			this.phoneNumber = phoneNumber;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getBlood() {
+		return blood;
+	}
+
+	public void setBlood(String blood) {
+		this.blood = blood;
+	}
+	
+	
 
 }
