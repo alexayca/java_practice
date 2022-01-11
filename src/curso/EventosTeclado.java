fa583704f8e9256691fbf395ec287daaff14db6d
AlexAC <90019860+alexayca@users.noreply.github.com>
2022-01-11 16:24:07 -0600

    71 . pildoras . Eventos del teclado

---
+++ src/curso/EventosTeclado.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
+public class EventosTeclado {
+    public static void main(String[] args) {
+        MarcoConTeclas myFrame = new MarcoConTeclas();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+
+/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
+class MarcoConTeclas extends JFrame{
+    public MarcoConTeclas(){
+        setVisible(true);
+        setBounds(700,300,600,450);
+        EventoDeTeclado tecla = new EventoDeTeclado();
+        addKeyListener(tecla);
+    }
+}
+
+
+/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
+class EventoDeTeclado implements KeyListener{
+    @Override
+    public void keyPressed(KeyEvent e) {
+        char letra = e.getKeyChar();
+        int codigo = e.getKeyCode();
+        System.out.println(codigo + " pressed "+ letra);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        char letra = e.getKeyChar();
+        int codigo = e.getKeyCode();
+        System.out.println(codigo + " released "+ letra);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        char letra = e.getKeyChar();
+        int codigo = e.getKeyCode();        // Retorna 0, siempre. Y algunas teclas no las retorna
+        System.out.println(codigo + " typed "+ letra);
+    }
+}
+
