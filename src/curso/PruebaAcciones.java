6ea4ec8379619122d92bde380d524729815d9a54
AlexAC <90019860+alexayca@users.noreply.github.com>
2022-01-12 01:41:06 -0600

    79 . pildoras . Eventos multiples fuentes IV

--- src/curso/PruebaAcciones.java
+++ src/curso/PruebaAcciones.java
@@ -5,6 +5,13 @@ import java.awt.event.ActionEvent;
 /* ** ** ** **
  * Eventos de multiples fuentes
  *  Clic o combinacion de teclas
+ *
+ * Asignando acciones al teclado
+ *  - Crear un mapa de entrada                  [InputMap   JComponent.getInputMap(int condicion)]
+ *  - Crear combinacion de teclas               static[KeyStroke.getKeyStroke(String s)]
+ *  - Asignar combinacion de teclas al objeto   [InputMap.put(keyStroke, Object)]
+ *  - Asignar objeto a accion                   [ActionMap.put(Object, accion)]
+ *
  *  */
 public class PruebaAcciones {
     public static void main(String[] args) {
@@ -37,11 +44,31 @@ class PanelAccion extends JPanel{
         AccionColor accionAmarillo = new AccionColor("Amarillo", new ImageIcon("src/ico/yellow.gif"), Color.YELLOW );
         AccionColor accionAzul = new AccionColor("Azul", new ImageIcon("src/ico/blue.gif"), Color.BLUE );
         AccionColor accionRojo = new AccionColor("Rojo", new ImageIcon("src/ico/red.png"), Color.RED );
-            // Creamos los botones y Agregamos los botones a la lamina
+            // Creamos los botones y Agregamos los botones a la lamina (objetos fuente botones)
         JButton botonAmarillo = new JButton(accionAmarillo);
         add(botonAmarillo);
         add(new JButton(accionAzul));
         add(new JButton(accionRojo));
+
+        // Fuente de combinacion de teclas
+            // Mapa de entrada
+        InputMap mapaEntrada = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);      // El objeto que recibe el foco es el boton, al estar dentro de la ventana del foco (dentro de la lamina)
+        //InputMap mapaEntrada = getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);  // Funciona
+        //InputMap mapaEntrada = getInputMap(JComponent.WHEN_FOCUSED);    // NO funciona, al ser el foco la propia lamina, Sin tocar ningun boton al iniciar funciona
+            // Crear combinacion de teclas
+        KeyStroke teclaAmarillo = KeyStroke.getKeyStroke("ctrl Y");
+            // Asignar la combinacion de teclas al objeto correspondiente
+        mapaEntrada.put(teclaAmarillo, "fondo_amarillo");
+        mapaEntrada.put(KeyStroke.getKeyStroke("ctrl A"), "fondo_azul");    // Se puede desencadenar la misma accion con otra combinacion de teclas
+        mapaEntrada.put(KeyStroke.getKeyStroke("ctrl B"), "fondo_azul");    // Combina y asigna
+        mapaEntrada.put(KeyStroke.getKeyStroke("ctrl R"), "fondo_rojo");    // Combina y asigna
+            // Asignar el objeto a la accion
+        ActionMap mapaAccion = getActionMap();
+        mapaAccion.put("fondo_amarillo", accionAmarillo);
+        mapaAccion.put("fondo_azul", accionAzul);
+        mapaAccion.put("fondo_rojo", accionRojo);
+
+
     }
 
     /* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
