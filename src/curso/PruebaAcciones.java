5c0ca6e51805d58b63a46b54fb1d2753cd8e00b0
AlexAC <90019860+alexayca@users.noreply.github.com>
2022-01-12 00:56:12 -0600

    78 . pildoras . Eventos multiples fuentes III

--- src/curso/PruebaAcciones.java
+++ src/curso/PruebaAcciones.java
@@ -23,7 +23,7 @@ class MarcoAccion extends JFrame{
     public MarcoAccion(){
             // Establecemos el marco principal
         setTitle("Prueba Acciones");
-        setBounds(600,350,600,300);
+        setBounds(600,350,900,500);
         PanelAccion lamina = new PanelAccion();
         add(lamina);
             //
@@ -37,31 +37,31 @@ class PanelAccion extends JPanel{
         AccionColor accionAmarillo = new AccionColor("Amarillo", new ImageIcon("src/ico/yellow.gif"), Color.YELLOW );
         AccionColor accionAzul = new AccionColor("Azul", new ImageIcon("src/ico/blue.gif"), Color.BLUE );
         AccionColor accionRojo = new AccionColor("Rojo", new ImageIcon("src/ico/red.png"), Color.RED );
-/*            // Creamos los botones
-        JButton botonAmarillo = new JButton("Amarillo");
-        JButton botonAzul = new JButton("Azul");
-        JButton botonRojo = new JButton("Rojo");
-            // Agregamos los botones a la lamina
+            // Creamos los botones y Agregamos los botones a la lamina
+        JButton botonAmarillo = new JButton(accionAmarillo);
         add(botonAmarillo);
-        add(botonAzul);
-        add(botonRojo);
-*/    }
+        add(new JButton(accionAzul));
+        add(new JButton(accionRojo));
+    }
+
+    /* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
+// Ponemos la lamina a la escucha, que se encargara de cambiar su color. Y la hacemos clase interna
+    private class AccionColor extends AbstractAction{
+        public AccionColor(String nombre, Icon icono, Color colorBoton) {
+            // Cuando se construya el objeto del evento se guardara clave: valor
+            putValue(Action.NAME, nombre);
+            putValue(Action.SMALL_ICON, icono);
+            putValue(Action.SHORT_DESCRIPTION, "Cambia el color de la lamina "+nombre);
+            putValue("Color_de_fondo", colorBoton);
+        }
+
+        @Override   // Se llama automaticamente cuando ocurre el evento
+        public void actionPerformed(ActionEvent e) {
+            Color c=(Color) getValue("Color_de_fondo"); // obtenemos el valor de la clave, de tipo objeto cast to color
+            setBackground(c);       // Corresponde a la clase JPanel
+            System.out.println("Name: " +getValue(Action.NAME) +". \nShort description: " +getValue(Action.SHORT_DESCRIPTION));
+        }
+    }
 }
 
 
-/* *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** */
-// Ponemos la lamina a la escucha, que se encargara de cambiar su color
-class AccionColor extends AbstractAction{
-    public AccionColor(String nombre, Icon icono, Color colorBoton) {
-        // Cuando se construya el objeto del evento se guardara clave: valor
-        putValue(Action.NAME, nombre);
-        putValue(Action.SMALL_ICON, icono);
-        putValue(Action.SHORT_DESCRIPTION, "Cambia el color de la lamina "+nombre);
-        putValue("Color_de_fondo", colorBoton);
-    }
-
-    @Override   // Se llama automaticamente cuando ocurre el evento
-    public void actionPerformed(ActionEvent e) {
-
-    }
-}
