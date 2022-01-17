4905cf8da1438c009146dd9662fcfd9036b0836a
alexayca <90019860+alexayca@users.noreply.github.com>
2022-01-16 20:40:09 -0600

    87 . pildoras . Swing.Cuadros de texto II

--- src/curso/CuadroTexto.java
+++ src/curso/CuadroTexto.java
@@ -1,3 +1,5 @@
+import jdk.dynalink.NamedOperation;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -21,21 +23,46 @@ class MarcoTexto extends JFrame{
 }
 
 class LaminaTexto extends JPanel{
-    private JTextField campo1 = new JTextField("Captura",10);
+    private JTextField campo1;
+    private JLabel resultado;
+
     public LaminaTexto() {
+        setLayout(new BorderLayout());      // Establece el BorderLayout para este panel
+        JPanel myPanel2 = new JPanel();
+        myPanel2.setLayout(new FlowLayout());// Estable el FlowLayout para myPanel2
+
+        resultado=new JLabel("Resultado",JLabel.CENTER);         // Inherited from interface SwingConstants
         JLabel texto1 = new JLabel("email:");
-        add(texto1);
-        add(campo1);
+        myPanel2.add(texto1);
+        campo1 = new JTextField("Captura",10);
+        myPanel2.add(campo1);
+        add(resultado, BorderLayout.CENTER);       // Agrega resultado en el centro del panel
         JButton myButton = new JButton("OK");
         IntroducirTexto myEvent = new IntroducirTexto();
         myButton.addActionListener(myEvent);
-        add(myButton);
+        myPanel2.add(myButton);     // Se agrega el boton a myPanel2
+        add(myPanel2, BorderLayout.NORTH);  // Agrega myPanel2 en el borde norte del panel
     }
 
     private  class IntroducirTexto implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println(campo1.getText().trim());    // Obtiene el texto y quita espacios en blanco
+            int arrobas=0;
+            String email=campo1.getText().trim();   // Obtiene el texto y quita espacios en blanco
+            for (int i=0;i<email.length();i++){
+                if (email.charAt(i)=='@'){
+                    arrobas++;
+                }
+            }
+            if (arrobas!=1){
+                String s ="Incorrecto";
+                resultado.setText(s);
+                System.out.println(s);
+            }else {
+                String s ="Correcto";
+                resultado.setText(s);
+                System.out.println(s);
+            }
         }
     }
 }
