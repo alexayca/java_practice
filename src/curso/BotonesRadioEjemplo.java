fcdd978d3743dbbbdaf8363200165b6dcb966497
AlexAC <90019860+alexayca@users.noreply.github.com>
2022-01-19 00:27:47 -0600

    94 . Swing. Botones de Radio II.2

--- src/curso/BotonesRadioEjemplo.java
+++ src/curso/BotonesRadioEjemplo.java
@@ -23,59 +23,44 @@ class LaminaRadioButtonE extends JPanel{
 
     private JLabel texto;
     private JRadioButton botonR1, botonR2, botonR3, botonR4;
+    private ButtonGroup miGrupo;
+    private JPanel laminaBotones;
 
     public LaminaRadioButtonE() {
         setLayout(new BorderLayout());
         texto=new JLabel("- Ya veo... - dijo el vampiro, pensativo, y lentamente cruzó la habitacion hacia la ventana.");
+        texto.setFont(new Font("Serif", Font.PLAIN, 12));
         add(texto, BorderLayout.CENTER);
 
-        ButtonGroup miGrupoBotones = new ButtonGroup();
-        botonR1=new JRadioButton("Pequeño", false);
-        botonR2=new JRadioButton("Mediano", true);
-        botonR3=new JRadioButton("Grande", false);
-        botonR4=new JRadioButton("Gigante", false);
+        // Las clases se instancian en el constructor, y no dentro del metodo colocarBotones
+        laminaBotones = new JPanel();
+        miGrupo = new ButtonGroup();
 
-        JPanel laminaRadio = new JPanel();
-        ManejaRadioButtonE eventoRadio = new ManejaRadioButtonE();
+        colocarBotones("Pequeño", false,10);
+        colocarBotones("Mediano", true,12);
+        colocarBotones("Grande", false,18);
+        colocarBotones("Gigante", false,26);
 
-        botonR1.addActionListener(eventoRadio);
-        botonR2.addActionListener(eventoRadio);
-        botonR3.addActionListener(eventoRadio);
-        botonR4.addActionListener(eventoRadio);
-
-        miGrupoBotones.add(botonR1);
-        miGrupoBotones.add(botonR2);
-        miGrupoBotones.add(botonR3);
-        miGrupoBotones.add(botonR4);
-
-        laminaRadio.add(botonR1);
-        laminaRadio.add(botonR2);
-        laminaRadio.add(botonR3);
-        laminaRadio.add(botonR4);
-
-        add(laminaRadio, BorderLayout.SOUTH);
-
-
+        add(laminaBotones, BorderLayout.SOUTH);
     }
 
-    // Clase interna para asociar el metodo actionPerformed, y agregar el listener a los RadioButtons
-    class ManejaRadioButtonE implements ActionListener{
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // Devuelve el origen del evento, quien desencadeno
-            if (e.getSource()==botonR1) {       /* Devuelve el desencadenate del objeto */
-                // System.out.println("Has pulsado el boton 1");
-                texto.setFont(new Font("Serif", Font.PLAIN, 10));
-            } else if (e.getSource()==botonR2){
-                texto.setFont(new Font("Serif", Font.PLAIN, 12));   // Es el tamaño y tipo por default, sin metodo
-            } else if (e.getSource()==botonR3){
-                texto.setFont(new Font("Serif", Font.PLAIN, 14));
-            } else if (e.getSource()==botonR4){
-                texto.setFont(new Font("Serif", Font.PLAIN, 20));
+    public void colocarBotones(String nombre, boolean seleccionado, final int tamanio){
+        JRadioButton boton = new JRadioButton(nombre, seleccionado);
+        miGrupo.add(boton);
+        laminaBotones.add(boton);
+        // Se instancio el listener (interfaz) dentro de metodo (interfaz interna)
+        // al hacer esto genera un problema, con tamanio el cual se requiere sea una constante
+        // para que no se pueda modificar, agregando final
+        ActionListener miEvento = new ActionListener(){
+            @Override
+                    public void actionPerformed(ActionEvent e){
+                        texto.setFont(new Font("Serif", Font.PLAIN, tamanio));
             }
+        };
+        boton.addActionListener(miEvento);
 
-
-        }
     }
+
+    // Se elimina la clase interna, porque no es necesaria para manejar los eventos
+
 }
