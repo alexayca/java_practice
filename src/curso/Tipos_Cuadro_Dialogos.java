dd981acebe3ce58598506542801361d45ec59472
alexayca <90019860+alexayca@users.noreply.github.com>
2022-02-27 00:58:18 -0600

    121. Ventanas emergentes. Caudros de dialogo I

---
+++ src/curso/Tipos_Cuadro_Dialogos.java
@@ -0,0 +1,66 @@
+package curso;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Tipos_Cuadro_Dialogos {
+    public static void main(String[] args) {
+        FrameCuadrosDialogos myFrame = new FrameCuadrosDialogos();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
+    }
+}
+
+
+class FrameCuadrosDialogos extends JFrame{
+    public FrameCuadrosDialogos(){
+        setBounds(500,300,400,250);
+        add(new PanelCuadroDialogo());
+    }
+}
+
+
+class PanelCuadroDialogo extends JPanel{
+
+    private JButton botonA, botonB, botonC, botonD;
+
+    public PanelCuadroDialogo() {
+
+        botonA = new JButton("Boton A");
+        botonB = new JButton("Boton B");
+        botonC = new JButton("Boton C");
+        botonD = new JButton("Boton D");
+
+        botonA.addActionListener(new AccionBotones());
+        botonB.addActionListener(new AccionBotones());
+        botonC.addActionListener(new AccionBotones());
+        botonD.addActionListener(new AccionBotones());
+
+        add(botonA);
+        add(botonB);
+        add(botonC);
+        add(botonD);
+    }
+
+    private class AccionBotones implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource()==botonA){
+                System.out.println("Pulsaste boton A");
+                // Indico que pertenece a una clase interna que se encuentra dentro del panel (clase donde se encuentra)
+                // solo this haria referencia a la clase privada
+                JOptionPane.showMessageDialog(PanelCuadroDialogo.this,"Ventana de mensaje de dialogo");
+            }else if (e.getSource()==botonB) {
+                System.out.println("Pulsaste boton B");
+                JOptionPane.showInputDialog(PanelCuadroDialogo.this,"Introduce el dato", "Introduccion datos",2);
+            }else if (e.getSource()==botonC) {
+                System.out.println("Pulsaste boton C");
+            }else {
+                System.out.println("Pulsaste boton D");
+            }
+
+        }
+    }
+}
+
