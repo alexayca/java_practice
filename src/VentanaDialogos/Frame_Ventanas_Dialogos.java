69b92ed69f89f90af97043cc0b7d5096fb17f5b2
alexayca <90019860+alexayca@users.noreply.github.com>
2022-02-27 19:33:57 -0600

    125.III Terminando interface grafica

--- src/VentanaDialogos/Frame_Ventanas_Dialogos.java
+++ src/VentanaDialogos/Frame_Ventanas_Dialogos.java
@@ -5,7 +5,8 @@ import java.awt.*;
 
 public class Frame_Ventanas_Dialogos extends JFrame {
 
-    private Panel_Buttons laminaTipo;
+    private Panel_Buttons laminaTipo, laminaTipoMensaje, laminaMensaje,
+                            laminaConfirmar, laminaOpcion, laminaEntrada;
 
     public Frame_Ventanas_Dialogos() throws HeadlessException {
 
@@ -17,8 +18,43 @@ public class Frame_Ventanas_Dialogos extends JFrame {
 
         String primerGrupoBox[]={"Mensaje", "Confirmar", "Opcion", "Entrada"};
         laminaTipo = new Panel_Buttons("Tipo", primerGrupoBox);
+
+        laminaTipoMensaje = new Panel_Buttons("Tipo de Mensaje", new String[]{
+                "ERROR_MESSAGE","INFORMATION_MESSAGE","QUESTION_MESSAGE","PLAIN_MESSAGE"
+        });
+
+        laminaMensaje = new Panel_Buttons("Mensaje", new String[]{
+                "Cadena","Icono","Componente","Otros","Object[]"
+        });
+
+        laminaConfirmar = new Panel_Buttons("", new String[]{
+                "DEFAULT_OPTION","YES_NO_OPTION","YES_NO_CANCEL_OPTION","OK_CANCEL_OPTION"
+        });
+
+        laminaOpcion = new Panel_Buttons("", new String[]{
+                "String[]","Icon[]","Object[]"
+        });
+
+        laminaEntrada = new Panel_Buttons("", new String[]{
+                "Campo de texto","Combo"
+        });
+
+        setLayout(new BorderLayout());
+
         panelSuperiorCuadricular.add(laminaTipo);
-        add(panelSuperiorCuadricular);
+        panelSuperiorCuadricular.add(laminaTipoMensaje);
+        panelSuperiorCuadricular.add(laminaMensaje);
+        panelSuperiorCuadricular.add(laminaConfirmar);
+        panelSuperiorCuadricular.add(laminaOpcion);
+        panelSuperiorCuadricular.add(laminaEntrada);
+
+        JPanel laminaBotonMostrar = new JPanel();
+        JButton botonMostrar = new JButton("Mostrar");
+        laminaBotonMostrar.add(botonMostrar);
+
+        add(panelSuperiorCuadricular, BorderLayout.CENTER);
+        add(laminaBotonMostrar,BorderLayout.SOUTH);
+
 
     }
 }
