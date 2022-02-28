c0e417fb06cee458b8acf19d251edfd3fc3c20f3
alexayca <90019860+alexayca@users.noreply.github.com>
2022-02-27 23:12:26 -0600

    130.VIII Botones de confirmacion

--- src/VentanaDialogos/Frame_Ventanas_Dialogos.java
+++ src/VentanaDialogos/Frame_Ventanas_Dialogos.java
@@ -35,15 +35,15 @@ public class Frame_Ventanas_Dialogos extends JFrame {
                 "Cadena","Icono","Componente","Otros","Object[]"
         });
 
-        laminaConfirmar = new Panel_Buttons("", new String[]{
+        laminaConfirmar = new Panel_Buttons("Confirmar", new String[]{
                 "DEFAULT_OPTION","YES_NO_OPTION","YES_NO_CANCEL_OPTION","OK_CANCEL_OPTION"
         });
 
-        laminaOpcion = new Panel_Buttons("", new String[]{
+        laminaOpcion = new Panel_Buttons("Opcion", new String[]{
                 "String[]","Icon[]","Object []"
         });
 
-        laminaEntrada = new Panel_Buttons("", new String[]{
+        laminaEntrada = new Panel_Buttons("Entrada", new String[]{
                 "Campo de texto","Combo"
         });
 
@@ -91,19 +91,18 @@ public class Frame_Ventanas_Dialogos extends JFrame {
         }
     }
 
-    // -------------------------devuelve tipo de icono----------------------------------
-    public int dameIcono(){
-        String s = laminaTipoMensaje.dameSeleccion();
-        if(s.equals("ERROR_MESSAGE")){
+    // -------------------------devuelve tipo de icono y numero de botones en confirmar----------------------------------
+    public int dameTipo(Panel_Buttons lamina){
+        String s = lamina.dameSeleccion();
+        if(s.equals("ERROR_MESSAGE") || s.equals("YES_NO_OPTION")){
             return 0;
-        }else if(s.equals("INFORMATION_MESSAGE")){
+        }else if(s.equals("INFORMATION_MESSAGE") || s.equals("YES_NO_CANCEL_OPTION")){
             return 1;
-        }else if(s.equals("WARNING_MESSAGE")){
+        }else if(s.equals("WARNING_MESSAGE") || s.equals("OK_CANCEL_OPTION")){
             return 2;
-        }
-        else if(s.equals("QUESTION_MESSAGE")){
+        }else if(s.equals("QUESTION_MESSAGE")){
             return 3;
-        }else if(s.equals("PLAIN_MESSAGE")){
+        }else if(s.equals("PLAIN_MESSAGE") || s.equals("DEFAULT_OPTION")){
             return -1;
         }else
             return 0;
@@ -116,13 +115,13 @@ public class Frame_Ventanas_Dialogos extends JFrame {
         public void actionPerformed(ActionEvent e) {
             //System.out.println(laminaTipo.dameSeleccion());
             if (laminaTipo.dameSeleccion().equals("Mensaje")){
-                JOptionPane.showMessageDialog(Frame_Ventanas_Dialogos.this,dameMensaje(),"Titulo",dameIcono());
+                JOptionPane.showMessageDialog(Frame_Ventanas_Dialogos.this,dameMensaje(),"Titulo", dameTipo(laminaTipoMensaje));
             }else if (laminaTipo.dameSeleccion().equals("Confirmar")){
-                JOptionPane.showConfirmDialog(Frame_Ventanas_Dialogos.this,dameMensaje(),"Titulo",0,dameIcono());
+                JOptionPane.showConfirmDialog(Frame_Ventanas_Dialogos.this,dameMensaje(),"Titulo",dameTipo(laminaConfirmar), dameTipo(laminaTipoMensaje));
             }else if(laminaTipo.dameSeleccion().equals("Opcion")){
-                JOptionPane.showOptionDialog(Frame_Ventanas_Dialogos.this, dameMensaje(),"Titulo",0,dameIcono(),null,null,null);
+                JOptionPane.showOptionDialog(Frame_Ventanas_Dialogos.this, dameMensaje(),"Titulo",0, dameTipo(laminaTipoMensaje),null,null,null);
             }else if (laminaTipo.dameSeleccion().equals("Entrada")){
-                JOptionPane.showInputDialog(Frame_Ventanas_Dialogos.this,dameMensaje(),"Titulo",dameIcono());
+                JOptionPane.showInputDialog(Frame_Ventanas_Dialogos.this,dameMensaje(),"Titulo", dameTipo(laminaTipoMensaje));
             }
         }
     }   // END private class
