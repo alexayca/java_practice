40f5e27e359e1998852bca287fa40ef5eb40c747
alexayca <90019860+alexayca@users.noreply.github.com>
2022-02-27 18:39:27 -0600

    123.I Marco principal y panel superior

--- src/Frame_Ventanas_Dialogos.java
+++ src/Frame_Ventanas_Dialogos.java
@@ -1,3 +1,13 @@
-public class Frame_Ventanas_Dialogos {
+import javax.swing.*;
+import java.awt.*;
 
+public class Frame_Ventanas_Dialogos extends JFrame {
+    public Frame_Ventanas_Dialogos() throws HeadlessException {
+        setTitle("Ejercicio de ventanas de dialogos");
+        setBounds(500,300,650,400);
+
+        JPanel panelSuperiorCuadricular = new JPanel();
+        panelSuperiorCuadricular.setLayout(new GridLayout(2,3));
+
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class main {
     public static void main(String[] args) {
+        Frame_Ventanas_Dialogos marcoDialogos = new Frame_Ventanas_Dialogos();
+        marcoDialogos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoDialogos.setVisible(true);
 
     }
 }
