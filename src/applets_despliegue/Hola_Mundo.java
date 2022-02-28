be234e2ea20abab438664c55c9a9f64ad61f9543
alexayca <90019860+alexayca@users.noreply.github.com>
2022-02-28 11:43:29 -0600

    anotaciones

--- src/applets_despliegue/Hola_Mundo.java
+++ src/applets_despliegue/Hola_Mundo.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Hola_Mundo extends JApplet {
 
     public void init(){
-        JLabel rotulo = new JLabel("Hola alumnos");
+        JLabel rotulo = new JLabel("Hola alumnos", SwingConstants.CENTER);
         add(rotulo);
     }
 }
--- src/lectura/despligue-de-aplicaciones.md
+++ src/lectura/despligue-de-aplicaciones.md
@@ -4,6 +4,7 @@
 Una aplicacion java que se ejecuta en un navegador de internet.
 Un applet no lleva metodo ```main()```, los applets sustituyen el arranque por un metodo ```init()```.
 La clase debe heredar de JApplet
+Es deprecated desde Java 9
 
 ## JAR
 
