93c27a7053cf6dacff99310e3b5ba3bdbe3876c6
alexayca <90019860+alexayca@users.noreply.github.com>
2022-03-16 10:43:49 -0600

    145. IV Excepciones

--- src/excepciones/EntradaDatos.java
+++ src/excepciones/EntradaDatos.java
@@ -1,5 +1,6 @@
 package excepciones;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -15,7 +16,16 @@ public class EntradaDatos {
         
         int decision = entrada.nextInt();
         if (decision==1) {
-            pedirDatos();
+            // La captura de la excepcion deberia lanzarse al momento de invocar al metodo
+            try {
+                pedirDatos();
+            }catch (Exception e){ /* IOException genera un objeto generico de la excepcion comprobada
+                aunque esta excepcion no tiene sentido, porque el programa nunca generara una excepcion
+                de este tipo, al no acceder a archivos externos. Debido a que la excepcion que genera es
+                 InputMissMatchException el programa se colgara. Siguiendo la herencia la excepcion mas
+                 generica es Exception */
+                System.out.println("Introduce un valor numerico");
+            }
         } else {
             System.out.println("Adios");
             System.exit(0);
@@ -25,10 +35,21 @@ public class EntradaDatos {
     }
 
     
-    // Para controlar se utilizara throws, aunque se considera mala practica de programacion
-    static void pedirDatos() throws InputMismatchException{
+    /* Para controlar se utilizara throws, aunque se considera mala practica de programacion
+        InputMismatchException hereda de RuntimeException por tanto es una excepcion no comprobada
+        y no es necesario utilizar try&catch para capturar la excepcion
+       En caso de usarse IOException si es requerido try&catch para capturar la excepcion
+       al ser una excepcion comprobada, pero en este caso es erroneo al no trabajar archivos
+    */
 
-        try{
+    static void pedirDatos() throws IOException {
+
+        /*  No es correcto si se usa un metodo que lanza una excepcion
+            la excepcion se debe capturar fuera del metodo. NO se debe capturar
+            la excepcion con try catch desde dentro del metodo
+            NO es coherente que el metodo lance la excepcion y tambien trate de capturarla
+        */
+
             Scanner entrada = new Scanner(System.in);
             
             System.out.println("Introduce tu nombre por favor");
@@ -43,10 +64,7 @@ public class EntradaDatos {
             System.out.println("Hola " + nombre_usuario +". El anio que viene tendras " +(edad+1) +" anios.");
 
             entrada.close();
-        }catch(InputMismatchException e){
-            // si se genera el error, se capturara en el objeto e
-            System.out.println("Introduce un numero");
-        }
+
         System.out.println("Hemos terminado.");
 
     }
