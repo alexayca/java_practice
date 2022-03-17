433374975d092d3d1357b4519b1559003297530a
alexayca <90019860+alexayca@users.noreply.github.com>
2022-03-16 21:23:29 -0600

    150 I Debugging

--- src/excepciones/Aleatorios.java
+++ src/excepciones/Aleatorios.java
@@ -2,6 +2,13 @@
 * Existen otro tipo de errores, del tipo logicos
 * para encontrarlos se usa el depurador de codigo
 *
+* Para comenzar a utilizar el debugger
+*   Se introduce un punto de interrupcion
+*   Se ejecuta el programa con el boton debug
+*       Se presentara la vista debug con panel debug, variables y breakpoints
+*    Step over : avanza una linea mas depues de la pausa
+*    Step Into
+*
 *
 * El programa llena una matriz con numeros aleatorios
 */
@@ -16,8 +23,10 @@ public class Aleatorios {
         int elementos = Integer.parseInt(JOptionPane.showInputDialog("Introduce elementos de la matriz"));
         int numAleatorio[]=new int[elementos];
 
+        // el programa no genera numeros aleatorios, solo ceros
         for (int i=0;i<numAleatorio.length;i++) {
-            numAleatorio[i]=(int)Math.random()*100;
+            // aqui se introduce el punto de interrupcion
+            numAleatorio[i]=(int)(Math.random()*100);
         }
 
         for (int elem : numAleatorio) {
