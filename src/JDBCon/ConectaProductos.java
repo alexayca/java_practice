1288fcc0e4da2837dbc0222fdcd85065007c3b75
alexayca <90019860+alexayca@users.noreply.github.com>
2022-03-18 11:47:58 -0600

    203 III JDBC.BBDD. Dudas

--- src/JDBCon/ConectaProductos.java
+++ src/JDBCon/ConectaProductos.java
@@ -10,7 +10,15 @@ public class ConectaProductos {
     public static void main(String[] args) {
 
         try {
+            /*  // Instruccion para asegurarse que el driver carga correctamente
+                // Inicia el driver con el metodo estatico forName()
+                // Devuelve la clase que se esta ejecutando para que cargue el driver
+            *   String driverName = "com.mysql.jdbc.Driver";
+            *   Class.forName(driverName); // here is the ClassNotFoundException
+            */
+
             // 1 crear conexion
+            // Puede obviarse el puerto si es por default
             Connection miConexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/test","alexa","");
             // 2 crear objeto statement
             Statement miStatement = miConexion.createStatement();
@@ -19,7 +27,8 @@ public class ConectaProductos {
             // 4 recorrer el resultset (mientras haya un registro mas)
             while (miResultset.next()){
                 // Convertimos a cadena de texto; con los nombres de cada columna como aparecen en la basede datos
-                System.out.println(miResultset.getString("id")+ "\t"+miResultset.getString("nombre") +"\t"+miResultset.getString("updatets"));
+                // getString no requiere poner el nombre de los campos, tan solo el entero que representa el orden de la columna; empezando en 1
+                System.out.println(miResultset.getShort(1) +"\t"+miResultset.getTimestamp(3) + "\t"+miResultset.getString("nombre"));
             }
 
         }catch (Exception e){
