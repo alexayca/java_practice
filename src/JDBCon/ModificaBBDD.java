48fece3b4f02b9bd274ba57b4800287d9f72beb8
alexayca <90019860+alexayca@users.noreply.github.com>
2022-03-18 22:33:06 -0600

    204 IV JDBC insert, delete, update

---
+++ src/JDBCon/ModificaBBDD.java
@@ -0,0 +1,33 @@
+package JDBCon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ModificaBBDD {
+
+    public static void main(String[] args) {
+
+        try {
+            //String driverName = "com.mysql.jdbc.Driver";
+            //Class.forName(driverName); // here is the ClassNotFoundException
+
+            Connection miConexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/test","alexa","");
+            Statement miStatement = miConexion.createStatement();
+
+            // Insertar registros
+            String instruccionInsertSQL = "INSERT INTO " +
+                    "deposito(nombre, id_alcaldia, id_colonia, dir_detalle) " +
+                    "VALUES('Patio Maravilla', 2, 246,'Poniente 152 y Av. maravillas, estacion la patera')";
+
+            miStatement.executeUpdate(instruccionInsertSQL);
+            System.out.println("Datos insertados");
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
