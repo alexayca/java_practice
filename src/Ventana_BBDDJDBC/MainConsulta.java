bce1e0d6cc6e46b803329bc2ebad76b79e982f24
alexayca <90019860+alexayca@users.noreply.github.com>
2022-04-20 22:28:44 -0500

    206.VI BBDD.JDBC

---
+++ src/Ventana_BBDDJDBC/MainConsulta.java
@@ -0,0 +1,13 @@
+package Ventana_BBDDJDBC;
+
+import javax.swing.*;
+
+public class MainConsulta {
+
+    public static void main(String[] args) {
+
+        JFrame myFrame = new FrameConsulta();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
+    }
+}
