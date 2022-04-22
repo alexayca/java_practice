61079b0d17232bda42d73055b0eb4fd2707f092c
alexayca <90019860+alexayca@users.noreply.github.com>
2022-04-21 19:05:09 -0500

    206.VI BBDD.JDBC items for comboBox

--- src/Ventana_BBDDJDBC/FrameConsulta.java
+++ src/Ventana_BBDDJDBC/FrameConsulta.java
@@ -2,6 +2,8 @@ package Ventana_BBDDJDBC;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.*;
+
 
 public class FrameConsulta extends JFrame {
 
@@ -36,6 +38,37 @@ public class FrameConsulta extends JFrame {
 
         JButton botonConsulta = new JButton("Consulta");
         add(botonConsulta, BorderLayout.SOUTH);
+
+
+        // --------------   Conexion a la base de datos  ----------------------------------------
+        try {
+            Connection myConnection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/test","alexa","");
+            Statement myStatementTest = myConnection.createStatement();
+
+            // extraemos las secciones para el primer comboBox (UNIQUE)
+            String query = "SELECT DISTINCTROW section FROM products";
+
+            // construimos el resultSet donde almacenar lo que devuelve la consulta
+            ResultSet rs=myStatementTest.executeQuery(query);
+
+            // Recorremos el resultSet, agregando los campos al comboBox secciones
+            while (rs.next()){
+                secciones.addItem(rs.getString(1));
+            }
+            // liberamos el resultSet
+            rs.close();
+
+            // --------- carga paises para el 2o comboBox----------
+            query = "SELECT DISTINCTROW country FROM products";
+            rs=myStatementTest.executeQuery(query);
+            while (rs.next()){
+                paises.addItem(rs.getString(1));
+            }
+            rs.close();
+
+        }catch (Exception e){
+
+        }
 
     }
 
