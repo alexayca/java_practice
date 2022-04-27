67ab920e2a8b43862ec6eabeb9c0eeaec6648916
alexayca <90019860+alexayca@users.noreply.github.com>
2022-04-26 19:57:58 -0500

    216.XVI BBDD.MVC.VII comboBox secciones y boton consulta

--- src/productos_controller/ControladorBotonEjecuta.java
+++ src/productos_controller/ControladorBotonEjecuta.java
@@ -2,6 +2,7 @@ package productos_controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
 import productos_model.*;
 import productos_view.*;
@@ -10,6 +11,7 @@ public class ControladorBotonEjecuta implements ActionListener {
 
     private MarcoAplicacion elmarco;
     EjecutaConsultas obj=new EjecutaConsultas();
+    private ResultSet resultadoConsulta;
 
     public ControladorBotonEjecuta(MarcoAplicacion elmarco){
         this.elmarco=elmarco;
@@ -19,7 +21,24 @@ public class ControladorBotonEjecuta implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String seleccionSeccion=(String) elmarco.secciones.getSelectedItem();
         String seleccionPaises=(String) elmarco.paises.getSelectedItem();
-        elmarco.resultado.append(obj.filtraBBDD(seleccionSeccion,seleccionPaises)); // return pruebas
-        elmarco.resultado.append("\n");
+        //elmarco.resultado.append(obj.filtraBBDD(seleccionSeccion,seleccionPaises)); // return pruebas
+        //elmarco.resultado.append("\n");
+        resultadoConsulta=obj.filtraBBDD(seleccionSeccion,seleccionPaises);
+        try {
+            elmarco.resultado.setText("");  // deja en blanco el cuadro de texto, con cada nueva consulta
+            while (resultadoConsulta.next()){
+                elmarco.resultado.append(resultadoConsulta.getString(1));
+                elmarco.resultado.append(" - ");
+                elmarco.resultado.append(resultadoConsulta.getString(2));
+                elmarco.resultado.append(" - ");
+                elmarco.resultado.append(resultadoConsulta.getString(3));
+                elmarco.resultado.append(" - ");
+                elmarco.resultado.append(resultadoConsulta.getString(4));
+                elmarco.resultado.append("\n");
+            }
+        }catch (Exception exception){
+
+        }
+
     }
 }
--- src/productos_model/EjecutaConsultas.java
+++ src/productos_model/EjecutaConsultas.java
@@ -1,18 +1,48 @@
 package productos_model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class EjecutaConsultas {
 
-    private String pruebas;
+    //private String pruebas;
+    private Conexion miConexion;
+    private ResultSet rs;
 
-    public String filtraBBDD(String seccion, String pais){
-        pruebas="";
-        if(!seccion.equals("Todos") && pais.equals("Todos")) {
-            pruebas="Has escogido seccion";
-        }else if(seccion.equals("Todos") && !pais.equals("Todos")){
-            pruebas="Has escogido pais";
-        }else  if(!seccion.equals("Todos") && !pais.equals("Todos")){
-            pruebas="Has escogido seccion y pais";
+    private PreparedStatement enviaConsultaSeccion;
+    private PreparedStatement enviaConsultaPais;
+    private PreparedStatement enviaConsultaTodos;
+
+    private final String consultaSeccion="SELECT articleName, section, price, country " +
+            "FROM products WHERE section=?";
+
+    public EjecutaConsultas(){
+        miConexion=new Conexion();
+
+    }
+
+    public ResultSet filtraBBDD(String seccion, String pais){
+        //pruebas="";
+        Connection conecta=miConexion.doConnection();
+        rs=null;
+
+        try {
+            if (!seccion.equals("Todos") && pais.equals("Todos")) {
+                //pruebas="Has escogido seccion";
+                enviaConsultaSeccion = conecta.prepareStatement(consultaSeccion);
+                enviaConsultaSeccion.setString(1,seccion);
+                rs=enviaConsultaSeccion.executeQuery();
+
+            } else if (seccion.equals("Todos") && !pais.equals("Todos")) {
+                //pruebas="Has escogido pais";
+            } else if (!seccion.equals("Todos") && !pais.equals("Todos")) {
+                //pruebas="Has escogido seccion y pais";
+            }
+            //return  pruebas;
+        }catch (Exception e){
+
         }
-        return  pruebas;
+        return rs;
     }
 }
