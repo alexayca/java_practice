81c528d2e94eba9b52632517c1f491ca3ee9a054
alexayca <90019860+alexayca@users.noreply.github.com>
2022-06-02 22:12:42 -0500

    2. Exercises

--- src/FischerBook/Drivers/SimpleFrameDriver.java
+++ src/FischerBook/Drivers/SimpleFrameDriver.java
@@ -4,10 +4,18 @@ import FischerBook.Its.SimpleFrame;
 
 public class SimpleFrameDriver {
     public static void main(String[] args) {
+
         SimpleFrame sFrame1 = new SimpleFrame();
         SimpleFrame sFrame2 = new SimpleFrame();
 
         sFrame1.showIt("Simple Frame 1");
         sFrame2.showIt("Simple Frame 2",300,300);
+
+        // app is redrawing
+        int k = 0;
+        while(true){
+            System.out.println("SimpleFrameDriver: Still running! "+k);
+            k++;
+        }
     }
 }
