7360f2735bbd8244bb55a05743b6d208326e09d4
alexayca <90019860+alexayca@users.noreply.github.com>
2022-07-07 21:52:56 -0500

    Applets in Swing Counter (deprecated)

---
+++ src/FischerBook/FirstGUI3/CounterApplet.java
@@ -0,0 +1,23 @@
+package FischerBook.FirstGUI3;
+import javax.swing.JApplet; // it's deprecated
+import java.awt.*;
+
+
+public class CounterApplet extends JApplet {
+    public void init(){
+        CounterPanel cPane=new CounterPanel();
+        this.getContentPane().add(cPane, BorderLayout.CENTER);
+    }
+
+    public void start(){
+        System.out.println("Start");
+    }
+
+    public void stop(){
+        System.out.println("Stop");
+    }
+
+    public void destroy(){
+        System.out.println("Destroy");
+    }
+}
