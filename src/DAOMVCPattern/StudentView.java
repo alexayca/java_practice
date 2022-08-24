15abb63b6a19c7c3da718b267a6f890d646fbe25
alexayca <90019860+alexayca@users.noreply.github.com>
2022-08-23 20:30:30 -0500

    example MVC

--- src/DAOMVCPattern/StudentView.java
+++ src/DAOMVCPattern/StudentView.java
@@ -4,4 +4,9 @@
 package DAOMVCPattern;
 
 public class StudentView {
+    public void printStudentDetails(String studentName, int studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
 }
